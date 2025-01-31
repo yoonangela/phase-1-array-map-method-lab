38e8b0ac10eaa5238ad5ff8ca21aa33833386145
Yeji-Yoon <angelayoon0102@gmail.com>
2025-01-31 10:54:49 -0800

    complete lab

--- index.js
+++ index.js
@@ -11,6 +11,54 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+
+
+
+
+function titleCased(){
+
+  return tutorials.map((element)=>
+    {makeSentence(capitalizeFirst(arrayOfwords(element)))
+    let wordsArray = arrayOfwords(element)  // Split the sentence into words
+    let capitalizedWords = capitalizeFirst(wordsArray)  // Capitalize the words
+    return makeSentence(capitalizedWords)}
+)
+
 }
+
+function arrayOfwords(element){
+  let spaceindex=0
+  let lastindex =0
+  let array = []
+  let word
+  for (const letter of element){
+    if (letter === " "){
+      word = element.slice(lastindex,spaceindex)
+      lastindex = spaceindex+1
+      array.push(word)    
+  }
+  spaceindex++
+ }
+ word = element.slice(lastindex);
+ if (word) array.push(word);  // Add the last word to the array if it exists
+ 
+ return array
+}
+
+function capitalizeFirst(array){
+  let capital
+  let capitalarray=[]
+  for (const words of array){
+    capital = words.slice(0,1).toUpperCase()+words.slice(1)
+    capitalarray.push(capital)
+  }
+  return capitalarray
+}
+
+function makeSentence(capitalarray){
+  return capitalarray.join(" ")
+  }
+
+
+console.log(titleCased())
